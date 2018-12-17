15e01d266784d62430f15052ab6e5ba04f4a9ffb
StephanHough <mr.swhough@gmail.com>
2018-12-17 20:35:33 +0000

    Hello World updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello World! I have just set up a CI/CD pipeline from GitHub to deploy Azure App Service.");
 
 });
 
