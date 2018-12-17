8f1aa74b81c60f3e096636757f3e41b49943d0e1
StephanHough <mr.swhough@gmail.com>
2018-12-17 21:09:50 +0000

    Home text updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! I have just set up a CI/CD pipeline from GitHub to deploy Azure App Services");
+    response.end("Hello World! Testing Azure/Github CI/CD pipeline from GitHub to deploy to Azure App Services");
 
 });
 
